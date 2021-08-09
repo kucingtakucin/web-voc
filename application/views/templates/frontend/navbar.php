eb4c54d5fbf1673012f0edc56bbf27e0f118f8fb
Taufik <hid09h@gmail.com>
2021-08-09 23:34:18 +0700

    Memperbaiki sweetalert

--- application/views/templates/frontend/navbar.php
+++ application/views/templates/frontend/navbar.php
@@ -3,11 +3,11 @@
 	</li>
 	<li class="nav-item"><a class="nav-link" style="font-weight: normal;" href="<?= base_url() ?>frontend/kompetisi">Kompetisi</a>
 	</li>
-	<li class="nav-item"><a class="nav-link live" style="font-weight: normal;" href="#!">
+	<li class="nav-item"><a class="nav-link live" style="font-weight: normal;" href="#">
 			<blink><i style="color: red;" class="icofont-youtube"></i></blink> Live Streaming
 		</a>
 	</li>
-	<li class="nav-item"><a class="nav-link klasemen" style="font-weight: normal;" href="#!">Klasemen</a>
+	<li class="nav-item"><a class="nav-link klasemen" style="font-weight: normal;" href="#">Klasemen</a>
 	</li>
 	<li class="nav-item"><a class="nav-link" style="font-weight: normal;" href="#!">Pendaftaran</a>
 	</li>
